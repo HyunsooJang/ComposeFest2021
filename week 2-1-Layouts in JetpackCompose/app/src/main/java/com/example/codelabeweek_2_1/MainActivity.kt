fc8fedc94a7e70ac9958ae254498387d994c1425
Mark <hyun8148@naver.com>
2021-11-14 16:31:51 +0900

    2-1 done

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/codelabeweek_2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/codelabeweek_2_1/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.codelabeweek_2_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -16,18 +14,19 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.FirstBaseline
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import coil.compose.rememberImagePainter
 import com.example.codelabeweek_2_1.ui.theme.CodeLabeWeek_21Theme
 import kotlinx.coroutines.launch
@@ -171,11 +170,18 @@ fun SimpleListPreview() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    MyOwnColumn(modifier = modifier.padding(8.dp)) {
-        Text("MyOwnColumn")
-        Text("places items")
-        Text("vertically.")
-        Text("We've done it by hand!")
+    Row(
+        modifier = modifier
+            .background(color = Color.LightGray, shape = RectangleShape)
+            .size(200.dp)
+            .padding(16.dp)
+            .horizontalScroll(rememberScrollState())
+    ) {
+        StaggeredGrid(modifier = modifier, rows = 5) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
     }
 }
 
@@ -240,8 +246,114 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Button(modifier: Modifier, onClick: (() -> Unit)? = null, content: @Composable () -> Unit) {
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(modifier = modifier, content = content) { measurables, constraints ->
 
+        val rowWidths = IntArray(rows) { 0 }
+
+        val rowHeight = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measuable ->
+            val placeable = measuable.measure(constraints = constraints)
+
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeight[row] = Math.max(rowHeight[row], placeable.height)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.minWidth)) ?: constraints.minWidth
+
+        val height =
+            rowHeight.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeight[i - 1]
+        }
+
+        layout(width = width, height = height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Stable
+fun Modifier.padding(all: Dp) = this.then(
+    PaddingModifier(start = all, top = all, end = all, bottom = all, rtlAware = true)
+)
+
+private class PaddingModifier(
+    var start: Dp = 0.dp,
+    var top: Dp = 0.dp,
+    var end: Dp = 0.dp,
+    var bottom: Dp = 0.dp,
+    var rtlAware: Boolean,
+) : LayoutModifier {
+
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+
+        val horizontal = start.roundToPx() + end.roundToPx()
+        val vertical = top.roundToPx() + bottom.roundToPx()
+
+        val placeable = measurable.measure(constraints.offset(-horizontal, -vertical))
+
+        val width = constraints.constrainWidth(placeable.width + horizontal)
+        val height = constraints.constrainHeight(placeable.height + vertical)
+        return layout(width, height) {
+            if (rtlAware) {
+                placeable.placeRelative(start.roundToPx(), top.roundToPx())
+            } else {
+                placeable.place(start.roundToPx(), end.roundToPx())
+            }
+        }
+    }
 }
 
 @Composable
@@ -249,10 +361,99 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+
+//        val (button1, button2, text) = createRefs()
+//
+//        Button(onClick = { /*TODO*/ },
+//            modifier = Modifier.constrainAs(button1) { top.linkTo(parent.top, margin = 16.dp) })
+//        {
+//            Text("Button1")
+//        }
+//
+//        Text(text = "Text", Modifier.constrainAs(text) {
+//            top.linkTo(button1.bottom, margin = 16.dp)
+//            centerHorizontallyTo(parent)
+//        })
+//
+//        val barrier = createEndBarrier(button1, text)
+//        Button(onClick = {},
+//        modifier = Modifier.constrainAs(button2){
+//            top.linkTo(parent.top, margin = 16.dp)
+//            start.linkTo(barrier)
+//        }) {
+//            Text(text = "Button2")
+//        }
+
+        val text = createRef()
+
+        val guideline = createGuidelineFromStart(0.5f)
+        Text(
+            "This is a very very very very very very very long text",
+            Modifier.constrainAs(text) {
+                linkTo(guideline, parent.end)
+                width = Dimension.preferredWrapContent
+            }
+        )
+    }
+}
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1 : String, text2 : String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+
+            text = text2
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CodeLabeWeek_21Theme {
         PhotographerCard()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChipPreview() {
+    CodeLabeWeek_21Theme {
+        Chip(text = "Hi there")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConstraintLayoutContentPreview() {
+    CodeLabeWeek_21Theme {
+        ConstraintLayoutContent()
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    CodeLabeWeek_21Theme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
     }
 }
